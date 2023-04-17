f85c9637be81c6fb6a00e8867bb3144b02765856
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-04-17 16:25:17 +0900

    01_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,8 +1,15 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
 
-GameScene::GameScene() {}
+GameScene::GameScene() : 
+	textureHandle_(0), 
+	soundDataHandle_(0),
+	voiceHandle_(0), 
+	inputFloat3() 
+{}
 
 GameScene::~GameScene() {}
 
@@ -11,9 +18,50 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	textureHandle_ = TextureManager::Load("./Resources/mario.jpg");
+	sprite_.reset(Sprite::Create(textureHandle_, {100, 50}));
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	soundDataHandle_ = audio_->LoadWave("./Resources/fanfare.wav");
+
+	 voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	 PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	 debugCamera_ = std::make_unique<DebugCamera>(1280, 720);
+
+	 AxisIndicator::GetInstance()->SetVisible(true);
+
+	 AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update()
+{
+	// スプライトの今の座標を表示
+	Vector2 position = sprite_->GetPosition();
+	// 座標を{ 2, 1 }移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	model.reset(Model::Create());
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(voiceHandle_);
+	}
+
+	ImGui::Begin("Debug1");
+	ImGui::InputFloat3("InputFloat3", inputFloat3.data());
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3.data(), 0.0f, 1.0f);
+	ImGui::End();
+
+	ImGui::ShowDemoWindow();
+
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -41,7 +89,16 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
+	for (int y = -360; y < 360; y+=2) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		    {-640.0f, float(y), 0.0f}, {640.0f, float(y), 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f});
+	}
+	for (int x = -640; x < 640; x+=2) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		    {(float)x, -360.0f, 0.0f}, {(float)x, 360.0f, 0.0f}, {0.0f, 1.0f, 0.0f, 1.0f});
+	}
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -53,6 +110,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,10 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include <memory>
+#include "ImGuiManager.h"
+#include <array>
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -48,4 +52,23 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	uint32_t textureHandle_;
+
+	std::unique_ptr<Sprite> sprite_;
+
+	std::unique_ptr<Model> model;
+
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	uint32_t soundDataHandle_;
+
+	uint32_t voiceHandle_;
+
+	std::array<float, 3> inputFloat3;
+
+	// デバックカメラ
+	std::unique_ptr<DebugCamera> debugCamera_;
 };
