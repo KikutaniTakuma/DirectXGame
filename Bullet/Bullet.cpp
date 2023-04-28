db355dabf80d35d927cbf77a958a251454364893
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-04-29 00:22:30 +0900

    変数名を適切なものに変更 リソースを追加

--- Bullet/Bullet.cpp
+++ Bullet/Bullet.cpp
@@ -15,17 +15,11 @@ void Bullet::Initialize(std::shared_ptr<Model> model, Vector3 pos) {
 void Bullet::Update() {
 	Vector3 move = { 0.0f,0.0f,0.0f };
 
-	const float kPlayerSpeed = 0.2f;
+	const float kBulletSpeed = 0.5f;
 
-	move.z = kPlayerSpeed;
-
-	const float kMoveLimitX = 34.0f;
-	const float kMoveLimitY = 19.0f;
+	move.z = kBulletSpeed;
 
 	worldTransform_.translation_ += move;
-
-	worldTransform_.translation_.x = std::clamp(worldTransform_.translation_.x, -kMoveLimitX, kMoveLimitX);
-	worldTransform_.translation_.y = std::clamp(worldTransform_.translation_.y, -kMoveLimitY, kMoveLimitY);
 
 	worldTransform_.matWorld_ = MakeMatrixAffin(Vector3(1.0f, 1.0f, 1.0f), Vector3(), worldTransform_.translation_);
 
